97aecd63008d336c0b53f4bddb25e83b00a2daba
Matt Friedman <maf675@gmail.com>
2024-12-29 11:44:54 -0800

    Add delete methods

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -104,6 +104,7 @@ class admin_controller
 		$submit = $this->request->is_set_post('submit');
 		$upload = $this->request->is_set_post('upload');
 		$resync = $this->request->is_set_post('resync');
+		$delete = $this->request->is_set_post('delete');
 
 		if ($submit || $upload || $resync)
 		{
@@ -124,6 +125,10 @@ class admin_controller
 			{
 				$this->save_settings();
 			}
+		}
+		else if ($delete)
+		{
+			$this->delete();
 		}
 
 		$this->display_settings();
@@ -243,6 +248,37 @@ class admin_controller
 		}
 
 		$this->success('CONFIG_UPDATED');
+	}
+
+	/**
+	 * Delete image
+	 *
+	 * @return void
+	 */
+	public function delete(): void
+	{
+		$path = $this->request->variable('delete', '');
+
+		if (confirm_box(true))
+		{
+			try
+			{
+				$this->helper->delete_icon($path);
+				$this->success($this->language->lang('ACP_PWA_IMG_DELETED', $path));
+			}
+			catch (runtime_exception $e)
+			{
+				$this->error($this->language->lang('ACP_PWA_IMG_DELETE_ERROR', $this->language->lang($e->getMessage())));
+			}
+		}
+		else
+		{
+			confirm_box(false, 'ACP_PWA_IMG_DELETE', build_hidden_fields(array(
+				'mode'		=> 'settings',
+				'delete'	=> $path,
+				'action'	=> $this->u_action,
+			)));
+		}
 	}
 
 	/**
