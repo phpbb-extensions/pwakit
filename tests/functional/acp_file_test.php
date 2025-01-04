ccafb791f9df7bed231e3fb241adb3e0958830b3
Matt Friedman <maf675@gmail.com>
2025-01-04 07:23:58 -0800

    Add more tests

--- tests/functional/acp_file_test.php
+++ tests/functional/acp_file_test.php
@@ -55,10 +55,10 @@ class acp_file_test extends phpbb_functional_test_case
 
 	private function upload_file($filename, $mimetype): Crawler
 	{
-		// Request index for correct URL
+		// Request ACP index for correct URL
 		self::request('GET', 'adm/index.php?sid=' . $this->sid);
 
-		// don't use adm, somehow self::$client->request remembers it from the admin log in
+		// self::$client->request remembers the adm/ part from the above request (or prior admin_login())
 		$url = 'index.php?i=-phpbb-pwakit-acp-pwa_acp_module&mode=settings&sid=' . $this->sid;
 
 		$crawler = self::$client->request('GET', $url);
@@ -82,72 +82,27 @@ class acp_file_test extends phpbb_functional_test_case
 		);
 	}
 
-//	public function test_empty_file()
-//	{
-//		$this->login();
-//
-//		$crawler = $this->upload_file('empty.png', 'image/png');
-//		$this->assertEquals($this->lang('EMPTY_FILEUPLOAD'), $crawler->filter('p.error')->text());
-//	}
-//
-//	public function test_invalid_extension()
-//	{
-//		$this->login();
-//
-//		$crawler = $this->upload_file('illegal-extension.bif', 'application/octet-stream');
-//		$this->assertEquals($this->lang('DISALLOWED_EXTENSION', 'bif'), $crawler->filter('p.error')->text());
-//	}
-//
-//	public function test_disallowed_content()
-//	{
-//		$this->login();
-//
-//		$crawler = $this->upload_file('disallowed.jpg', 'image/jpeg');
-//		$this->assertEquals($this->lang('DISALLOWED_CONTENT'), $crawler->filter('p.error')->text());
-//	}
-//
-//	public function test_disallowed_content_no_check()
-//	{
-//		$this->login();
-//		$this->admin_login();
-//		$this->add_lang('ucp');
-//
-//		// Make sure check_attachment_content is set to false
-//		$crawler = self::request('GET', 'adm/index.php?sid=' . $this->sid . '&i=acp_attachments&mode=attach');
-//
-//		$form = $crawler->selectButton('Submit')->form(array(
-//			'config[check_attachment_content]'	=> 0,
-//		));
-//		self::submit($form);
-//
-//		// Request index for correct URL
-//		self::request('GET', 'index.php?sid=' . $this->sid);
-//
-//		$crawler = $this->upload_file('disallowed.jpg', 'image/jpeg');
-//
-//		// Hitting the UNABLE_GET_IMAGE_SIZE error means we passed the
-//		// DISALLOWED_CONTENT check
-//		$this->assertContainsLang('UNABLE_GET_IMAGE_SIZE', $crawler->text());
-//
-//		// Reset check_attachment_content to default (enabled)
-//		$crawler = self::request('GET', 'adm/index.php?sid=' . $this->sid . '&i=acp_attachments&mode=attach');
-//
-//		$form = $crawler->selectButton('Submit')->form(array(
-//			'config[check_attachment_content]'	=> 1,
-//		));
-//		self::submit($form);
-//	}
-//
-//	public function test_too_large()
-//	{
-//		$this->create_user('fileupload');
-//		$this->login('fileupload');
-//
-//		$crawler = $this->upload_file('too-large.png', 'image/png');
-//		$this->assertEquals($this->lang('WRONG_FILESIZE', '256', 'KiB'), $crawler->filter('p.error')->text());
-//	}
+	public function test_upload_empty_file()
+	{
+		$this->login();
+		$this->admin_login();
 
-	public function test_valid_file()
+		$crawler = $this->upload_file('empty.png', 'image/png');
+
+		$this->assertEquals($this->lang('EMPTY_FILEUPLOAD'), $crawler->filter('div.errorbox > p')->text());
+	}
+
+	public function test_upload_invalid_extension()
+	{
+		$this->login();
+		$this->admin_login();
+
+		$crawler = $this->upload_file('foo.gif', 'image/gif');
+
+		$this->assertEquals($this->lang('DISALLOWED_EXTENSION', 'gif'), $crawler->filter('div.errorbox > p')->text());
+	}
+
+	public function test_upload_valid_file()
 	{
 		$this->login();
 		$this->admin_login();
