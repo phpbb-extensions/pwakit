90de924337e9b620f025ad41c3cc716fdb2fe15c
Matt Friedman <maf675@gmail.com>
2025-01-18 16:54:39 -0800

    Extend track_files

--- storage/file_tracker.php
+++ storage/file_tracker.php
@@ -10,9 +10,33 @@
 
 namespace phpbb\pwakit\storage;
 
+use phpbb\cache\driver\driver_interface as cache;
+use phpbb\config\config;
+use phpbb\db\driver\driver_interface as db;
+
 class file_tracker extends \phpbb\storage\file_tracker
 {
 	public const STORAGE_NAME = 'phpbb_pwakit';
+
+	protected config $config;
+
+	protected string $phpbb_root_path;
+
+	/**
+	 * Constructor
+	 *
+	 * @param cache $cache
+	 * @param db $db
+	 * @param string $storage_table
+	 * @param config $config
+	 * @param string $phpbb_root_path
+	 */
+	public function __construct(cache $cache, db $db, string $storage_table, config $config, string $phpbb_root_path)
+	{
+		parent::__construct($cache, $db, $storage_table);
+		$this->config = $config;
+		$this->phpbb_root_path = $phpbb_root_path;
+	}
 
 	/**
 	 * Gets tracked files in the storage table
@@ -29,5 +53,22 @@ class file_tracker extends \phpbb\storage\file_tracker
 		$this->db->sql_freeresult($result);
 
 		return array_column($files, 'file_path');
+	}
+
+	public function track_files(string $storage, array $files): void
+	{
+		$files_ary = [];
+		foreach ($files as $file)
+		{
+			$files_ary[] = [
+				'file_path'	=> $file,
+				'filesize'	=> filesize($this->phpbb_root_path . $this->config['storage\\phpbb_pwakit\\config\\path'] . '/' . $file),
+			];
+		}
+
+		if (!empty($files_ary))
+		{
+			parent::track_files($storage, $files_ary);
+		}
 	}
 }
